9f076d5d6c68df00371c24f50a62bdaad920f6ea
ArthurFloro <arthur.floro2006@gmail.com>
2025-11-16 01:36:53 -0300

    adicao da rota POST para envio da URL longa

---
+++ src/routes/url.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const {nanoid} = require('nanoid')
+const Url = require('../models/Url')
+
+// rota POST
+// Cria uma URL curta
+router.posrt('/shorten', async (req, res) => {
+    const {longUrl} = req.body
+
+    // criando um baseUrl considerando que seria o nosso proprio servidor
+    const baseUrl = `http://localhost:${process.env.PORT}`
+
+    //validando a URL
+    if (!longUrl) {
+        return res.status(400).json('Por Favor, forneça uma URL')
+    }
+
+    try {
+        // verificando se a URL ja existe no banco de dados
+        let url = await Url.findOne({longUrl})
+
+        if (url) {
+            //se ja existir, retorna apenas os dados dela
+            return res.json(url)
+        }
+
+        // se nao existir, cria uma nova URL curta
+        
+        // gerando um codigo unico para a URL curta
+        const shortCode = nanoid(5) // gerando um ID de 5 caracteres
+
+        // monta a URL completa
+        const shortUrl = `${baseUrl}/${shortCode}`
+
+        // cria o novo objeto URL
+        url = new Url({
+            longUrl, 
+            shortCode, 
+            shortUrl, 
+            date: new Date()
+        })
+
+        // salvando no banco de dados
+        await url.save()
+
+        // retorna os dados para o cliente
+        res.json(url)
+
+    } catch (e) {
+        console.error(e)
+        res.status(500).json('Erro no servidor')
+    }
+})
+
+module.exports = router
